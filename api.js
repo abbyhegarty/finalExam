77c75014bdd62e085cd67a40fce52962d982a6cc
abbyhegarty <54848927+abbyhegarty@users.noreply.github.com>
2019-12-07 22:44:35 -0500

    Create api.js

---
+++ api.js
@@ -0,0 +1,29 @@
+var data =
+[
+   {
+   "name":"Iron",
+   "description":"burns the fair folk",
+   "image":"http://fairy.web/pics/iron.jpg"
+   },
+{
+   "name":"Bells",
+   "description":"that Ringing sound!",
+   "image":"http://fairy.web/pics/liberty.jpg"
+   },
+{
+   "name":"Red Ribbons",
+   "description":"dangerously funny",
+   "image":"http://fairy.web/pics/firstPrize.jpg"
+   },
+{
+   "name":"Iron",
+   "description":"burns the fair folk",
+   "image":"http://fairy.web/pics/iron.jpg"
+   },
+{
+   "name":"salt",
+   "description":"Stings when you cross it",
+   "image":"http://fairy.web/pics/saltFlats.jpg"
+   },
+]
+
